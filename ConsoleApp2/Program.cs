ab1cb2af08c269290a1bf707840c914b57df94c8
nasta15 <48784865+nasta15@users.noreply.github.com>
2019-03-21 12:26:31 +0500

    Сalculator

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, d;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            d = a + b;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
